dee206772ba829be43e6999c23bcd0554b498ed3
Zachary Milletich <106951331+weddedlime@users.noreply.github.com>
2024-01-27 09:54:07 -0500

    first edition of lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,18 @@
-print("hello world")
+print("Hello World")
+print("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+print("Hi! This is a double quote mark: \" We use it for printing.")
+print("Audrey Hepburn once said \"Nothing is impossible. The word itself says 'I'm Possible!'.\"")
+print("The file is stored in C:\\new folder")
+print("This\nis\nmy\nsample.")
+print("This \n is \n my \n sample.")
+print("""You can print
+on multiple
+lines using
+triple
+quotes.""")
+print("""You can print
+        on multiple
+        lines using
+        triple
+        quotes.""")
