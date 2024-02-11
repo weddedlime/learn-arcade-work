6343d2df94dca3a0419c3810ab964acd8f944fc6
Zachary Milletich <106951331+weddedlime@users.noreply.github.com>
2024-02-11 13:22:26 -0500

    lab 3

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -12,34 +12,33 @@ def draw_square(x, y):
 
     arcade.draw_rectangle_filled(x, y, 40, 40, arcade.color.GREEN)
 
-def main():
 
-    arcade.open_window(600, 600, "Drawing Functions Example")
-    arcade.set_background_color(arcade.color.WHITE)
-
-
-    draw_circle(100, 100)
+arcade.open_window(600, 600, "Drawing Functions Example")
+arcade.set_background_color(arcade.color.WHITE)
 
 
-    draw_triangle(300, 300)
+draw_circle(100, 100)
 
 
-    draw_square(500, 500)
+draw_triangle(300, 300)
 
 
-    draw_circle(400, 200)
+draw_square(500, 500)
 
 
-    draw_square(200, 400)
+draw_circle(400, 200)
 
 
-    draw_triangle(100, 500)
+draw_square(200, 400)
 
 
-    arcade.finish_render()
+draw_triangle(100, 500)
 
 
-    arcade.run()
+arcade.draw_point(100, 100, arcade.color.BLACK, 5)
 
 
-main()
+arcade.finish_render()
+
+
+arcade.run()
