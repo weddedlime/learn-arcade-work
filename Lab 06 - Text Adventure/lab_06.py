fd91ff1464f97138e4d3160ff6beec15a537b443
Zachary Milletich <106951331+weddedlime@users.noreply.github.com>
2024-04-10 12:47:02 -0400

    lab 6

--- Lab 06 - Text Adventure/lab_06.py
+++ Lab 06 - Text Adventure/lab_06.py
@@ -1,59 +1,62 @@
 import arcade
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Room Adventure"
+
+
 class Room:
-    def __init__(self, name="", description="", north=None, east=None, south=None, west=None):
-        self.name = name
+    def __init__(self, description="", north=None, east=None, south=None, west=None):
         self.description = description
         self.north = north
         self.east = east
         self.south = south
         self.west = west
 
-class GameWindow(arcade.Window):
+
+class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        self.room_list = []
-
-        # Creating rooms
-        room = Room("Starting Room", "You are in the starting room. There is a passage to the north.", 1, 2, 3, 4)
-        self.room_list.append(room)
-
-        room = Room("Second Room", "You are in another room. There is a passage to the south.", None, None, 0, None)
-        self.room_list.append(room)
-
-        room = Room("East Room", "You are in a room with a door to the east.", None, None, None, 0)
-        self.room_list.append(room)
-
-        room = Room("West Room", "You are in a room. There is a passage to the west.", 0, None, None, None)
-        self.room_list.append(room)
-
         self.current_room = 0
+        self.rooms = []
+        self.setup()
+
+    def setup(self):
+        # Room 0
+        room = Room("You are in a brightly lit kitchen. There is a door to the north.", north=1)
+        self.rooms.append(room)
+
+        # Room 1
+        room = Room("You are in a spacious living room with a cozy fireplace. A kitchen lies to the south.",
+                    south=0)
+        self.rooms.append(room)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text(self.room_list[self.current_room].name, 20, 500, arcade.color.WHITE, 20)
-        arcade.draw_text(self.room_list[self.current_room].description, 20, 450, arcade.color.WHITE, 12)
+        room_description = self.rooms[self.current_room].description
+        arcade.draw_text(room_description, 50, SCREEN_HEIGHT // 2, arcade.color.BLACK, 12, align="center",
+                         anchor_x="center", anchor_y="center", width=700)
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.ESCAPE:
-            arcade.close_window()
-        elif key == arcade.key.UP:
-            self.move("north")
+        current_room = self.rooms[self.current_room]
+        if key == arcade.key.UP:
+            if current_room.north is not None:
+                self.current_room = current_room.north
         elif key == arcade.key.DOWN:
-            self.move("south")
+            if current_room.south is not None:
+                self.current_room = current_room.south
         elif key == arcade.key.LEFT:
-            self.move("west")
+            if current_room.west is not None:
+                self.current_room = current_room.west
         elif key == arcade.key.RIGHT:
-            self.move("east")
+            if current_room.east is not None:
+                self.current_room = current_room.east
 
-    def move(self, direction):
-        next_room = getattr(self.room_list[self.current_room], direction)
-        if next_room is not None:
-            self.current_room = next_room
 
 def main():
-    window = GameWindow(800, 600, "Arcade Room Game")
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
